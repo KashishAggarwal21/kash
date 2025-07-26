5dffce43a2aa48b9adc2b495d362cecbb6da4ff7
KashishAggarwal21 <kashulagoyal789@gmail.com>
2025-07-26 23:30:00 +0530

    Add files via upload

---
+++ dsa2array.cpp
@@ -0,0 +1,39 @@
+//linear serach
+
+// #include<iostream>
+// using  namespace std;
+// int linearsearch(int*arr, int n,int key){
+// for(int i=0;i<n;i++){
+//     if(arr[i]== key){
+//         return i;
+//     }
+
+// }
+// return -1;
+// }
+
+//reverse array.
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int arr[] = {5,4,3,9,2};
+//     int n = sizeof (arr)/sizeof(int);
+
+//     int copyArr[n];
+//     for(int i=0;i<n;i++){
+//         int j=n-1-i;
+//         copyArr[i] = arr[j];
+//     }
+//     for(int i=0;i<n;i++){
+//         arr[i] = copyArr[i];
+//     }
+//     return 0;
+
+// }
+
+
+
+
+//reverse array with extra space (2 pointer approach)
+
